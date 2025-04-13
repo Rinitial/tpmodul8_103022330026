17c85523d8f8aa8e0b009f5e056428f355afdf63
Raditya <mp.raditya93w@gmail.com>
2025-04-14 01:35:29 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_103022330026/CovidConfig.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_103022330026
+{
+    class CovidConfig
+    {
+        // Path untuk menyimpan file konfigurasi
+        public static string filePath = Path.Combine(Directory.GetCurrentDirectory(), "covid_config.json");
+
+        [JsonPropertyName("satuan_suhu")] // 
+        public string SatuanSuhu { get; set; }
+        [JsonPropertyName("batas_hari_demam")]
+        public int BatasDemam { get; set; }
+        [JsonPropertyName("pesan_ditolak")]
+        public string PesanDitolak { get; set; }
+        [JsonPropertyName("pesan_diterima")]
+        public string PesanDiterima { get; set; }
+
+        // Constructor parameterless default untuk default config
+        public CovidConfig()
+        {
+            setDefault();
+        }
+
+        public CovidConfig(string satuan_suhu, int batas_hari_demam, string pesan_ditolak, string pesan_diterima)
+        // construktor untuk config baru
+        {
+            this.SatuanSuhu = satuan_suhu;
+            this.BatasDemam = batas_hari_demam;
+            this.PesanDitolak = pesan_ditolak;
+            this.PesanDiterima = pesan_diterima;
+        }
+
+        public void UbahSatuan()
+        // Method untuk mengubah satuan suhu
+        {
+            if (SatuanSuhu == "celcius")
+            {
+                SatuanSuhu = "fahrenheit";
+            }
+            else
+            {
+                SatuanSuhu = "celcius";
+            }
+            // Simpan perubahan ke file
+            SaveNewConfig();
+        }
+
+        public void loadConfig()
+        // Method untuk memuat konfigurasi dari file
+        {
+            // pengecekan file
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    // Membaca file JSON
+                    string config_json = File.ReadAllText(filePath);
+                    // Menggunakan JsonSerializer untuk mendeserialisasi JSON ke objek CovidConfig
+                    CovidConfig configFromFile = JsonSerializer.Deserialize<CovidConfig>(config_json);
+
+                    // Perbarui nilai properti dengan hasil dari file
+                    SatuanSuhu = configFromFile.SatuanSuhu;
+                    BatasDemam = configFromFile.BatasDemam;
+                    PesanDitolak = configFromFile.PesanDitolak;
+                    PesanDiterima = configFromFile.PesanDiterima;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Terjadi kesalahan: " + ex.Message);
+                }
+            }
+            else
+            {
+                // Jika file tidak ditemukan, simpan objek yang sudah memiliki default ke file
+                SaveNewConfig();
+                Console.WriteLine("File tidak ditemukan, membuat file baru");
+            }
+        }
+
+        public void SaveNewConfig()
+        {
+            try
+            {
+                // Menggunakan JsonSerializer untuk serialisasi objek ke JSON
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                // Menyimpan JSON ke file
+                string jsonString = JsonSerializer.Serialize(this, options);
+                // Menulis JSON ke file
+                File.WriteAllText(filePath, jsonString);
+                Console.WriteLine("Berhasil menyimpan konfigurasi baru");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Gagal menyimpan konfigurasi baru: " + ex.Message);
+            }
+        }
+
+        private void setDefault()
+        // Method untuk mengatur nilai default
+        {
+            this.SatuanSuhu = "celcius";
+            this.BatasDemam = 14;
+            this.PesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+            this.PesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+        }
+
+        public string GetMessage(string status)
+        // Method untuk mendapatkan pesan berdasarkan status
+        {
+            return status == "ditolak" ? PesanDitolak : PesanDiterima;
+        }
+    }
+}
--- tpmodul8_103022330026/Program.cs
+++ tpmodul8_103022330026/Program.cs
@@ -1,1 +1,64 @@
-﻿
+﻿using System;
+using tpmodul8_103022330026;
+
+namespace tpmodul8_103022330026
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Membuat Objeck CovidConfig
+            var config = new CovidConfig();
+            // Memamnggil Method loadConfig
+            config.loadConfig();
+
+
+            Console.WriteLine("Selamat datang di aplikasi pemeriksaan Covid-19");
+            Console.WriteLine($"Satuan suhu saat ini: {config.SatuanSuhu}");
+            Console.Write("Apakah anda ingin mengubah satuan suhu? (Y/N) : ");
+            string ubahSatuan = Console.ReadLine();
+            if (ubahSatuan.ToLower() == "y")
+            {
+                config.UbahSatuan();
+                Console.WriteLine($"Satuan suhu diubah menjadi: {config.SatuanSuhu}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pertanyaan");
+            Console.Write($"Berapa suhu badan anda saat ini? (dalam nilai {config.SatuanSuhu}): ");
+            double suhu = Convert.ToDouble(Console.ReadLine());
+
+            if (config.SatuanSuhu == "celcius")
+            {
+                if (suhu < 36.5 || suhu > 37.5)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(config.GetMessage("ditolak"));
+                    return;
+                }
+            }
+            else
+            {
+                if (suhu < 97.7 || suhu > 99.5)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(config.GetMessage("ditolak"));
+                    return;
+                }
+            }
+
+            Console.Write("Berapa hari yang lalu anda terakhir memiliki gejala demam? : ");
+            int hari = Convert.ToInt32(Console.ReadLine());
+            if (hari <= config.BatasDemam)
+            {
+                Console.WriteLine();
+                Console.WriteLine(config.GetMessage("ditolak"));
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine(config.GetMessage("diterima"));
+            }
+        }
+    }
+}
